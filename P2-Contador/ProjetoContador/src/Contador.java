e525f0152b50bdd9f6ce6eb284913adf6e48cce2
Luiz Felipe <luiz@felipe.com>
2024-06-04 22:11:12 -0300

    Criado o projeto Java para o segundo projeto que será desenvolvido um contador e também já criado o scanner

--- P2-Contador/ProjetoContador/src/Contador.java
+++ P2-Contador/ProjetoContador/src/Contador.java
@@ -1,3 +1,13 @@
-public class Contador {
+import java.util.Scanner;
 
+public class Contador {
+    public static void main(String[] args) {
+        Scanner resposta = new Scanner(System.in);
+        int primeiroNum, segundoNum;
+
+        System.out.println("Digite o primeiro número: ");
+        primeiroNum = resposta.nextInt();
+        System.out.println("Digite o segundo número: ");
+        segundoNum = resposta.nextInt();
+    }
 }
