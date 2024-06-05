b45ba012fae8f202c0cf5ede1d74b73dc5c5bfed
Luiz Felipe <luiz@felipe.com>
2024-06-04 22:26:50 -0300

    Desenvolvido a primeira tentativa para verificar se o código funcionará

--- P2-Contador/ProjetoContador/src/Contador.java
+++ P2-Contador/ProjetoContador/src/Contador.java
@@ -9,5 +9,23 @@ public class Contador {
         primeiroNum = resposta.nextInt();
         System.out.println("Digite o segundo número: ");
         segundoNum = resposta.nextInt();
+
+        try{
+            contar(primeiroNum, segundoNum);
+        }catch (Exception NumeroInvalido){
+            if (primeiroNum > segundoNum) {
+                System.out.println("O primeiro número precisa ser menor que o segundo!");
+            }            
+        }
+
+        resposta.close();
+    }
+
+    static void contar(int primeiroNum, int segundoNum) throws Exception{
+        int contagem = primeiroNum - segundoNum;
+
+        for(int i = 0; i < contagem; i++) {
+            System.out.println("Imprimindo o número " + contagem);
+        }
     }
 }
