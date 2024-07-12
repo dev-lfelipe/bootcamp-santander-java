4b22705b9485a3f866908277df3a0ae5465f6ff3
Luiz Felipe <luiz@felipe.com>
2024-07-12 17:22:31 -0300

    Atualizado o formato da data da mentoria para o padrão brasileiro (dia/mês/ano)

--- P5-AbstracaoBootcamp/src/dominio/Mentoria.java
+++ P5-AbstracaoBootcamp/src/dominio/Mentoria.java
@@ -1,9 +1,11 @@
 package dominio;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Mentoria extends Conteudo {
     private LocalDate data;
+    DateTimeFormatter formatada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     @Override
     public double calcularXp(){
@@ -24,6 +26,12 @@ public class Mentoria extends Conteudo {
     public String toString(){
         return "Título: " + getTitulo() + '\n' +
                 "Descrição da Mentoria: " + getDescricao() + '\n' +
-                "Data da Mentoria: " + data + '.';
+                "Data da Mentoria: " + data.format(formatada) + '.';
     }
+
+    /* Código que encontrei para servir de base para a formatação da data:
+    LocalDateTime now = LocalDateTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    System.out.println("Data formatada: " + now.format(formatter));
+    */
 }
